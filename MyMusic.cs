47ded252caf494fdedd2ac018135dc7bacb764ea
Skorlup <zuykov.2002@mail.ru>
2019-12-07 13:40:26 +0300

    ...

---
+++ MyMusic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Spisok
+    {
+        public Sakas[] Sborka { get; set; }
+    }
+    class Sakas
+    {
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public string Title { get; set; }
+        public string Year { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Spisok myCollection = new Spisok();
+            myCollection.Sborka = new Sakas[3];
+
+            myCollection.Sborka[0] = new Sakas()
+            {
+                Artist = "Artist1",
+                Album = "Album1",
+                Title = "Title1",
+                Year = "2015"
+            };
+            myCollection.Sborka[1] = new Sakas()
+            {
+                Artist = "Artist2",
+                Album = "Album2",
+                Title = "Title2",
+                Year = "2015"
+            };
+            myCollection.Sborka[2] = new Sakas()
+            {
+                Artist = "Artist3",
+                Album = "Album3",
+                Title = "Title3",
+                Year = "2015"
+            };
+
+
+            string serialized = JsonConvert.SerializeObject(myCollection);
+
+            Console.WriteLine(serialized);
+            File.WriteAllText(@"C:\Users\z_m_1\source\repos\ConsoleApp8\ConsoleApp8\List.txt", serialized);
+            Console.ReadKey();
+        }
+    }
+}
